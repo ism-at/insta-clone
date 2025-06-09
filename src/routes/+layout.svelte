a54a525c9ef5d1b207b93e3186a2d52c4e530378
ismail <alagele.ismail@gmail.com>
2025-06-10 00:53:34 +0200

    E

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,16 +1,3 @@
-<!-- <script lang='ts'>
-    import '../app.css';
-    // Supports weights 100-900
-    import '@fontsource-variable/inter';
-    import { ModeWatcher } from "mode-watcher";
-    let {children} = $props();
-</script> -->
-
-<!-- NOTE: Rendering the children is equivalence to SLOT -->
-<!-- {@render children()} 
-
-<ModeWatcher/> -->
-
 <script lang="ts">
 	import '../app.css';
 	import '@fontsource-variable/inter';
@@ -19,7 +6,3 @@
 
 <slot />
 <ModeWatcher />
-
-
-
-
