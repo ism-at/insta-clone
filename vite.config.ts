af4a5a8ec39c8ffb64dd76311fa797b9aa6df122
ismail <alagele.ismail@gmail.com>
2025-06-13 11:56:07 +0200

    Fix vite in vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,5 +2,9 @@ import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [sveltekit()]
+	plugins: [sveltekit()],
+	// Vite didn't detected
+	css: {
+		postcss: './postcss.config.js'
+	}
 });
