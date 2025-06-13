387a5a2e9a7e749d6d06cc42796935d4bbf761a0
ismail <alagele.ismail@gmail.com>
2025-06-13 15:19:05 +0200

    Update

---
+++ postcss.config.cjs
@@ -0,0 +1,9 @@
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const tailwindConfig = require('./tailwind.config.cjs');
+
+module.exports = {
+  plugins: {
+    tailwindcss: { config: tailwindConfig },
+    autoprefixer: {}
+  }
+};
